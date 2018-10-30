0033d14bd18ef169a2b826929ab238205c54810a
rhoden23 <40925312+rhoden23@users.noreply.github.com>
2018-10-29 22:39:37 -0700

    Initial Commit

---
+++ InstagramClone/Controller/ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  InstagramClone
+//
+//  Created by Roden on 10/28/18.
+//  Copyright © 2018 Roden. All rights reserved.
+//
+
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        NotificationCenter.default.post(name: NSNotification.Name("didLogout"), object: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
